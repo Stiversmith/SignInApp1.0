86ffe6e2202756c0d491e419b369be57cde0006c
Aliaksandr Yashchuk <alexander@stiversmith.com>
2023-08-25 19:43:23 -0400

    Project created

---
+++ SignInApp1.0/SignInApp1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignInApp1.0
+//
+//  Created by Aliaksandr Yashchuk on 8/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
